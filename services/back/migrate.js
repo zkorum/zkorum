15fb6178238527ead86ed188f26e9cc615c58ab6
Nicolas Gimenez <46368291+nicobao@users.noreply.github.com>
2023-07-22 01:47:15 +0200

    db: delete drizzle migrate

--- services/back/migrate.js
+++
@@ -1,8 +0,0 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import { migrate } from "drizzle-orm/postgres-js/migrator";
-import postgres from "postgres";
-
-const sql = postgres("...", { max: 1 });
-const db = drizzle(sql);
-
-await migrate(db, { migrationsFolder: "drizzle" });
