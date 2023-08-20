e4a4c82c8a8c96e6ab5bc892eb1bb21572367245
Robin Marien <robin.marien@viveris.fr>
2023-08-20 15:07:18 +0200

    first commit of all times

--- services/front/src/components/dashboard/Dashboard.tsx
+++ services/front/src/components/dashboard/Dashboard.tsx
@@ -7,7 +7,7 @@ export function Dashboard() {
         .map(
           () => `Cras mattis consectetur purus sit amet fermentum.
 Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+Morbi leo risus, porta ac consectetur ac, vestibulam at eros.
 Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
         )
         .join("\n")}
