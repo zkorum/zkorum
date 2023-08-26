1cb7e6e9c9e0457d32eb801b542feb8563130d83
Nicolas Gimenez <46368291+nicobao@users.noreply.github.com>
2023-08-26 18:54:54 +0200

    chore: add prettier

--- services/front/.eslintrc.cjs
+++ services/front/.eslintrc.cjs
@@ -4,6 +4,7 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
+    "prettier",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
