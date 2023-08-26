2d4f32265b31e77d2d95162af08bc4e804b9e608
Robin Marien <robin.marien@viveris.fr>
2023-08-26 17:04:06 +0200

    first layer postView

--- services/front/src/components/dashboard/Dashboard.tsx
+++ services/front/src/components/dashboard/Dashboard.tsx
@@ -3,7 +3,7 @@ import { PostView } from "./PostView";
 import { Divider } from "@mui/material";
 
 export function Dashboard() {
-  const posts = [{id: 1, title: "Post 1", type: 2}, {id: 2, title: "Post 2", type: 1}]
+  const posts = Array(1000).fill({title: "Do you feel safe on Campus ?", type: 2, viewNumber: 65, comments: Array(20).fill("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book"), description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", isEligible: true, expiredOn: "20/09/2023", participants: 593 })
   const indexLastPost = posts.length - 1
   console
   return (
--- services/front/src/components/dashboard/PostView.tsx
+++ services/front/src/components/dashboard/PostView.tsx
@@ -1,6 +1,10 @@
 import Grid from "@mui/material/Unstable_Grid2/Grid2";
-
-
+import ApartmentIcon from '@mui/icons-material/Apartment';
+import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
+import PublicIcon from '@mui/icons-material/Public';
+import PollIcon from '@mui/icons-material/Poll';
+import CommentIcon from '@mui/icons-material/Comment';
+import VisibilityIcon from '@mui/icons-material/Visibility';
 interface PostProps {
   post: Post;
 }
@@ -9,22 +13,35 @@ interface Post {
   id: number;
   title: string;
   type: number;
+  isEligible: boolean;  
+  expiredOn: string;
+  participants: number;
+  description: string;
+  viewNumber: number;
+  comments: [];
 }
 
 export function PostView(props: PostProps) {
   return (
     // lines
-    <Grid container>
+    <Grid container spacing={2}>
       {/* line 1 - group logo and other info */}
-      <Grid container>{props.post.title}</Grid>
+      <Grid xs={8}><ApartmentIcon/>{"ESSEC"}</Grid>
+      <Grid xs={4}>{props.post.isEligible ? <AdminPanelSettingsIcon /> : null}<span>2h</span><PublicIcon/></Grid>
       {/* line 2 - poll/vote title */}
-      <Grid>{props.post.type}</Grid>
-      {/* line 3 - poll/vote type */}
-      <Grid>{props.post.id}</Grid>
-      {/* line 4 - tags */}
-      <Grid></Grid>
-      {/* line 5 - comments and view counts */}
-      <Grid></Grid>
+      <Grid xs={12}>{props.post.title}</Grid>
+      {/* line 3 - exerpt */}
+      <Grid xs={12}>{props.post.description.slice(0, 60) + "..."}</Grid>
+      {/* line 4 - poll/vote type */}
+      <Grid xs={12}>{props.post.id}</Grid>
+      {/* line 5 - tags */}
+      <Grid xs={1}>{props.post.type === 2 ? <PollIcon/> : null}</Grid>
+      <Grid xs={2}>{"60% No"}</Grid>
+      <Grid xs={4}>{props.post.participants + " participants"}</Grid>
+      <Grid xs={4}>{"open indefinitely"}</Grid>
+      {/* line 6 - comments and view counts */}
+      <Grid xs={6}><CommentIcon/>{props.post.comments.length}</Grid>
+      <Grid xs={6}><VisibilityIcon/>{props.post.viewNumber}</Grid>
     </Grid>
   );
 }
