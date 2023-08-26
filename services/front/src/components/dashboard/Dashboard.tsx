137e6171b15e0905d87988ee7707ef106718a41e
Robin Marien <robin.marien@viveris.fr>
2023-08-26 16:11:16 +0200

    Work in progress

--- services/front/src/components/dashboard/Dashboard.tsx
+++ services/front/src/components/dashboard/Dashboard.tsx
@@ -4,13 +4,14 @@ import { Divider } from "@mui/material";
 
 export function Dashboard() {
   const posts = [{id: 1, title: "Post 1", type: 2}, {id: 2, title: "Post 2", type: 1}]
-  const isLastPost = posts.length - 1
+  const indexLastPost = posts.length - 1
+  console
   return (
     <Box sx={{ my: 2 }}>
-      {posts.map((post) => {
+      {posts.map((post, index) => {
         return <>
         <PostView post={post}/>
-        {isLastPost ? null : <Divider/>
+        {indexLastPost === index ? null : <Divider />
         }</>
       })}
     </Box>
