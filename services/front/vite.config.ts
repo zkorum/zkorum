f6f6fed38e8ef7fb8992918ba4cb16873abbc82d
Nicolas Gimenez <46368291+nicobao@users.noreply.github.com>
2023-12-13 09:42:29 +0100

    fix: remove PWA for now

--- services/front/vite.config.ts
+++ services/front/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import { ValidateEnv } from "@julr/vite-plugin-validate-env";
-import { VitePWA } from "vite-plugin-pwa";
 import svgr from "vite-plugin-svgr";
 import react from "@vitejs/plugin-react-swc";
 import checker from "vite-plugin-checker";
@@ -10,7 +9,6 @@ import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfil
 export default defineConfig({
     plugins: [
         react(),
-        VitePWA({ registerType: "autoUpdate" }),
         svgr(),
         checker({
             typescript: true,
