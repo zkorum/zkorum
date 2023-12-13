187b8fe0c3a01f734e5fa26474d5983dd6b557d0
Nicolas Gimenez <46368291+nicobao@users.noreply.github.com>
2023-12-13 15:19:13 +0100

    fix: put pwa back...

--- services/front/vite.config.ts
+++ services/front/vite.config.ts
@@ -4,11 +4,13 @@ import svgr from "vite-plugin-svgr";
 import react from "@vitejs/plugin-react-swc";
 import checker from "vite-plugin-checker";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { VitePWA } from "vite-plugin-pwa";
 import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 
 export default defineConfig({
     plugins: [
         react(),
+        VitePWA({ registerType: "autoUpdate" }),
         svgr(),
         checker({
             typescript: true,
