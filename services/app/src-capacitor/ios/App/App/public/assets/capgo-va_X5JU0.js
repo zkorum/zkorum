2055acacd98441e5444f48d08f18eaa3d4ebbff8
Nicolas Gimenez <46368291+nicobao@users.noreply.github.com>
2024-05-06 00:52:17 +0200

    fix: remove ios folder

--- services/app/src-capacitor/ios/App/App/public/assets/capgo-va_X5JU0.js
+++
@@ -1,2 +0,0 @@
-const __vite__fileDeps=["./web-Dj1ruBXQ.js","./index-Dx07DNJy.js","./index-CSum7Adq.css"],__vite__mapDeps=i=>i.map(i=>__vite__fileDeps[i]);
-import{k as a,_ as e,j as o}from"./index-Dx07DNJy.js";const r=a("CapacitorUpdater",{web:()=>e(()=>import("./web-Dj1ruBXQ.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(t=>new t.CapacitorUpdaterWeb)}),i=o(()=>{r.notifyAppReady()});export{i as default};
