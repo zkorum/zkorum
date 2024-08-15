08c4a5b6712dccae8fe1a70b906080a9d5befcd9
Edmund Wong <9814980+e32wong@users.noreply.github.com>
2024-08-15 05:38:25 -0400

    Fix the route transition by moving them from layout views to the App root

--- services/afterwork/src/App.vue
+++ services/afterwork/src/App.vue
@@ -1,5 +1,15 @@
 <template>
-  <router-view />
+  <!--<router-view />-->
+
+  <router-view v-slot="{ Component }">
+    <transition appear enter-active-class="animated fadeIn slow" leave-active-class="animated fadeOut" mode="out-in">
+      <component :is="Component" />
+    </transition>
+  </router-view>
+
+  <!--
+  <TransitionRouterView />
+  -->
 </template>
 
 <script setup lang="ts">
