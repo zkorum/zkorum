cf147361f40890a7c1e13cd1101d51581bc42e5f
Edmund Wong <9814980+e32wong@users.noreply.github.com>
2024-08-18 10:45:06 -0400

    Simplify ZKButton prop passthrough

--- services/agora/src/components/ui-library/ZKButton.vue
+++ services/agora/src/components/ui-library/ZKButton.vue
@@ -1,29 +1,9 @@
 <template>
-  <q-btn :label="label" :type="type" :to="to" :color="color" :text-color="textColor" :icon="definedIcon" no-caps
-    :rounded="true" unelevated />
+  <q-btn v-bind="props" color="deep-purple" text-color="white" no-caps :rounded="true" unelevated />
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { QBtnProps } from "quasar";
 
-const props = defineProps({
-  label: String,
-  to: [String, Object],
-  icon: String,
-  color: {
-    type: String,
-    default: "deep-purple"
-  },
-  textColor: String,
-  type: String
-});
-
-const definedIcon = ref<undefined | string>(undefined);
-const flat = ref(false);
-
-if (props.icon != undefined) {
-  definedIcon.value = props.icon;
-  flat.value = true;
-}
-
+const props = defineProps<QBtnProps>();
 </script>
