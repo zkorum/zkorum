410bd697c344f285e00f53add4902709c1b2635a
Edmund Wong <9814980+e32wong@users.noreply.github.com>
2024-08-18 12:20:12 -0400

    Add sticky button to homepage

--- services/agora/src/pages/index.vue
+++ services/agora/src/pages/index.vue
@@ -1,5 +1,12 @@
 <template>
-  <CompactPostList />
+  <div>
+    <CompactPostList />
+  </div>
+
+  <q-page-sticky position="bottom-right" :offset="[18, 18]">
+    <q-btn fab icon="add" color="accent" unelevated />
+  </q-page-sticky>
+
 </template>
 
 <script setup lang="ts">
