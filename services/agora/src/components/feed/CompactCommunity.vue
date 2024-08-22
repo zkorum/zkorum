d0422ce2a5c105125fb61d0808358838237f2124
Edmund Wong <9814980+e32wong@users.noreply.github.com>
2024-08-22 14:59:24 -0400

    Style the compact community list with shadows and hover effects

--- services/agora/src/components/feed/CompactCommunity.vue
+++ services/agora/src/components/feed/CompactCommunity.vue
@@ -2,17 +2,18 @@
   <div>
     <div class="container">
       <swiper-container slidesPerView="4.5">
-        <swiper-slide v-for="communityItem in communityList" v-bind:key="communityItem.emoji">
-          <RouterLink :to="{ name: 'community-single', params: { countryCode: communityItem.code.toLowerCase() } }">
-            <div class="communityItemStyle">
-              <div class="flagStyle">
-                <img class="flagImage" :src="'/images/communities/flags/' + communityItem.code + '.svg'" />
-              </div>
-              <div class="flagCountryName">
-                {{ communityItem.countryName }}
+        <swiper-slide v-for="communityItem in communityList" v-bind:key="communityItem.name">
+          <RouterLink :to="{ name: 'community-single', params: { countryCode: communityItem.name } }">
+            <div class="outerPadding">
+              <div class="communityItemStyle">
+                <div class="flagStyle">
+                  <img class="flagImage" :src="'/images/communities/flags/' + communityItem.code + '.svg'" />
+                </div>
+                <div class="flagCountryName">
+                  {{ communityItem.countryName }}
+                </div>
               </div>
             </div>
-
           </RouterLink>
         </swiper-slide>
       </swiper-container>
@@ -23,39 +24,39 @@
 <script setup lang="ts">
 
 interface FlagItem {
-  emoji: string
+  name: string
   countryName: string
   code: string
 }
 
 const communityList: FlagItem[] = [
   {
-    "emoji": "🌎",
+    "name": "world",
     "countryName": "World",
     "code": "WORLD"
   },
   {
-    "emoji": "🇫🇷",
+    "name": "france",
     "countryName": "France",
     "code": "FR"
   },
   {
-    "emoji": "🇨🇳",
+    "name": "china",
     "countryName": "China",
     "code": "CN"
   },
   {
-    "emoji": "🇺🇸",
+    "name": "united-states",
     "countryName": "United States",
     "code": "US"
   },
   {
-    "emoji": "🇷🇺",
+    "name": "russia",
     "countryName": "Russia",
     "code": "RU"
   },
   {
-    "emoji": "🇯🇵",
+    "name": "japan",
     "countryName": "Japan",
     "code": "JP"
   }
@@ -63,7 +64,7 @@ const communityList: FlagItem[] = [
 
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 a {
   text-decoration: none;
 }
@@ -78,12 +79,21 @@ a {
   align-items: center;
   justify-content: center;
   gap: 0.5rem;
-  padding: 1rem;
+  padding: 0.5rem;
+}
+
+.communityItemStyle:hover {
+  border-radius: 15px;
+  background-color: rgb(0 0 0 / 0.08);
+  box-shadow: 0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1);
 }
 
 .flagImage {
   width: 3rem;
   border-radius: 5px;
+  border-style: solid;
+  border-width: 1px;
+  border-color: #e5e7eb;
 }
 
 .flagCountryName {
@@ -95,5 +105,9 @@ a {
   display: flex;
   align-items: center;
   justify-content: center;
+}
+
+.outerPadding {
+  padding: 0.5rem;
 }
 </style>
