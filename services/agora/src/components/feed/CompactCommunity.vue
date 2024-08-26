2425056bb6ef45da9a9f4bbd5e89ce1c88ed8b2c
Edmund Wong <9814980+e32wong@users.noreply.github.com>
2024-08-26 09:25:47 -0400

    Remove country names from the compact community cluster

--- services/agora/src/components/feed/CompactCommunity.vue
+++ services/agora/src/components/feed/CompactCommunity.vue
@@ -4,8 +4,8 @@
       <swiper-container slidesPerView="4.5">
         <swiper-slide v-for="communityName in communityList" v-bind:key="communityName">
           <RouterLink :to="{ name: 'community-single', params: { countryCode: communityName } }">
-            <div class="outerPadding">
-              <CommunityIcon :communityName="communityName" :showCountryName="true" class="communityItem" />
+            <div class="outerPadding hoverEffect">
+              <CommunityIcon :communityName="communityName" :showCountryName="false" />
             </div>
           </RouterLink>
         </swiper-slide>
@@ -30,13 +30,13 @@ a {
   border-radius: 15px;
 }
 
-.communityItem:hover {
+.hoverEffect:hover {
   border-radius: 15px;
   background-color: rgb(0 0 0 / 0.08);
   box-shadow: 0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1);
 }
 
 .outerPadding {
-  padding: 0.5rem;
+  padding: 1rem;
 }
 </style>
