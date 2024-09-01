fb5c03fdb79310d601430e9501e329512d130bc6
Edmund Wong <9814980+e32wong@users.noreply.github.com>
2024-09-01 18:55:45 -0400

    Implement a new post wrapper that positions based on the parent width

---
+++ services/agora/src/components/feed/NewPostButtonWrapper.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div>
+      <slot />
+
+      <q-btn unelevated class="floatButton" fab icon="mdi-plus" color="accent" @click="buttonClick()" />
+
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+const emit = defineEmits(["onClick"]);
+
+function buttonClick() {
+  emit("onClick")
+}
+
+</script>
+
+<style scoped>
+.floatButton {
+  position: fixed;
+  bottom: 4rem;
+  right: calc(calc(100vw - calc(min(40rem, 100%))) / 2 + 1rem);
+}
+</style>
