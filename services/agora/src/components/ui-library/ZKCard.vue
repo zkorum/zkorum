8bd2958d1280849f0e6a41bce078682eeab873e5
Edmund Wong <9814980+e32wong@users.noreply.github.com>
2024-09-02 09:38:48 -0400

    Fix comment section's scoped styling

--- services/agora/src/components/ui-library/ZKCard.vue
+++ services/agora/src/components/ui-library/ZKCard.vue
@@ -13,11 +13,11 @@
 
 <style scoped lang="scss">
 .card-decoration {
-  background-color: #ecfeff;
+  background-color: #f5f5f4;
   border-radius: 15px;
   border-style: solid;
   border-width: 1px;
-  border-color: #67e8f9;
+  border-color: #e7e5e4;
   box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px,
     rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
   padding: 1rem;
