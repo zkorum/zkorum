fd2d6bb28088b66ddc2c0b6b3e4f94c859f7b9de
Edmund Wong <9814980+e32wong@users.noreply.github.com>
2024-09-03 15:53:21 -0400

    Transition the single-post page to use the data store

--- services/agora/src/pages/a/[communityId]/post/[postSlugId].vue
+++ services/agora/src/pages/a/[communityId]/post/[postSlugId].vue
@@ -5,10 +5,20 @@
 </template>
 
 <script setup lang="ts">
-import { usePostStore } from "@/stores/post";
+import { DummyPostDataFormat, usePostStore } from "@/stores/post";
 import PostItem from "@/components/feed/PostItem.vue";
+import { useRoute } from "vue-router";
 
-const postData = usePostStore().generateDummyPostData(0);
+const route = useRoute();
+
+const postStore = usePostStore();
+
+let postData: DummyPostDataFormat = postStore.emptyPost;
+
+const postSlugId = route.params.postSlugId;
+if (typeof postSlugId == "string") {
+  postData = postStore.getPostBySlugId(postSlugId);
+}
 
 </script>
 
