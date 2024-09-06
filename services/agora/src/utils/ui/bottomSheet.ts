675cee8d09916dc9a79290db109d4a151a2c4277
Edmund Wong <9814980+e32wong@users.noreply.github.com>
2024-09-05 22:29:38 -0400

    Add dialogs for the report system

--- services/agora/src/utils/ui/bottomSheet.ts
+++ services/agora/src/utils/ui/bottomSheet.ts
@@ -35,5 +35,47 @@ export const useBottomSheet = () => {
     })
   }
 
-  return { showCreatePostCommunitySelector };
+
+  // Return the selected community name
+  function showPostOptionSelector(grid: boolean) {
+
+    const actionList = [];
+
+    actionList.push({
+      label: "Report",
+      icon: "mdi-flag",
+      id: "report"
+    });
+
+    quasar.bottomSheet({
+      message: "Select an action for this post",
+      grid,
+      actions: actionList
+    }).onOk(action => {
+      console.log("Selected action: " + action.id);
+      if (action.id == "report") {
+        processReportAction();
+      }
+    }).onCancel(() => {
+      console.log("Dismissed");
+    }).onDismiss(() => {
+      // console.log('I am triggered on both OK and Cancel')
+    })
+  }
+
+  function processReportAction() {
+    quasar.dialog({
+      title: "Report System",
+      message: "Would you like to report this post?",
+      cancel: true,
+      persistent: false,
+    }).onOk(() => {
+      quasar.dialog({
+        title: "Thank you for the report",
+        message: "We will investigate if the clip contains inappropriate content",
+      });
+    });
+  }
+
+  return { showCreatePostCommunitySelector, showPostOptionSelector };
 }
