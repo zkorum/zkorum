76229bcac1d70da8b4dce320323231b72ad57f0c
Edmund Wong <9814980+e32wong@users.noreply.github.com>
2024-09-11 18:09:12 -0400

    Add format free pasting for the editor

--- services/agora/src/components/post/views/CommentComposer.vue
+++ services/agora/src/components/post/views/CommentComposer.vue
@@ -2,7 +2,13 @@
   <div>
     <WidthWrapper>
       <div class="container">
-        <q-editor v-model="commentText" placeholder="Add a comment" min-height="2rem" flat ref="editorRef" />
+        <form autocorrect="off" autocapitalize="off" autocomplete="off" spellcheck="false">
+          <q-editor v-model="commentText" placeholder="Add a comment" min-height="2rem" flat ref="editorRef" :toolbar="[
+            ['bold', 'italic', 'strike', 'underline'],
+            ['undo', 'redo']
+          ]" @paste="onPaste" />
+        </form>
+
         <!--@update:model-value="checkWordCount()"-->
         <div class="postButton">
           <ZKButton label="Cancel" color="secondary" @click="cancelClicked()" />
@@ -14,7 +20,7 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
+import { ref } from "vue";
 import WidthWrapper from "src/components/navigation/WidthWrapper.vue";
 import ZKButton from "src/components/ui-library/ZKButton.vue";
 
@@ -24,9 +30,34 @@ const commentText = ref("");
 
 const editorRef = ref();
 
-onMounted(() => {
-  editorRef.value.focus();
-})
+function onPaste(evt: Event) {
+  // Let inputs do their thing, so we don't break pasting of links.
+  /* @ts-expect-error Event definition is missing */
+  if (evt.target?.nodeName === "INPUT") return
+  let text, onPasteStripFormattingIEPaste
+  evt.preventDefault()
+  evt.stopPropagation()
+  /* @ts-expect-error Event definition is missing */
+  if (evt.originalEvent && evt.originalEvent.clipboardData.getData) {
+    /* @ts-expect-error Event definition is missing */
+    text = evt.originalEvent.clipboardData.getData("text/plain")
+    editorRef.value.runCmd("insertText", text)
+  }
+  /* @ts-expect-error Event definition is missing */
+  else if (evt.clipboardData && evt.clipboardData.getData) {
+    /* @ts-expect-error Event definition is missing */
+    text = evt.clipboardData.getData("text/plain")
+    editorRef.value.runCmd("insertText", text)
+  }
+  /* @ts-expect-error Definition is missing */
+  else if (window.clipboardData && window.clipboardData.getData) {
+    if (!onPasteStripFormattingIEPaste) {
+      onPasteStripFormattingIEPaste = true
+      editorRef.value.runCmd("ms-pasteTextOnly", text)
+    }
+    onPasteStripFormattingIEPaste = false
+  }
+}
 
 function cancelClicked() {
   emit("cancelClicked")
