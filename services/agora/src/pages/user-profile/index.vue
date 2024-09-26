0ae63a84012e28accd937bb01d64969166a39829
Edmund Wong <9814980+e32wong@users.noreply.github.com>
2024-09-26 17:20:08 -0400

    Add dummy pages for user profile

--- services/agora/src/pages/user-profile/index.vue
+++ services/agora/src/pages/user-profile/index.vue
@@ -1,11 +1,79 @@
 <template>
   <div>
-    User Profile
+
+    <div class="profileDetails">
+      <div>
+        100 comments <span class="dotPadding">•</span>
+      </div>
+      <div>
+        1000 votes <span class="dotPadding">•</span>
+      </div>
+      <div>
+        Jan 1, 2024
+      </div>
+    </div>
+
+    <Tabs value="0">
+      <TabList>
+        <Tab value="0">Comments</Tab>
+        <Tab value="1">Votes</Tab>
+      </TabList>
+      <TabPanels>
+        <TabPanel value="0">
+          <p class="m-0">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+            est laborum.
+          </p>
+        </TabPanel>
+        <TabPanel value="1">
+          <p class="m-0">
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem
+            aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+            Nemo enim
+            ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
+            qui ratione voluptatem sequi nesciunt. Consectetur, adipisci velit, sed quia non numquam eius modi.
+          </p>
+        </TabPanel>
+        <TabPanel value="2">
+          <p class="m-0">
+            At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti
+            atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique
+            sunt in culpa
+            qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et
+            expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.
+          </p>
+        </TabPanel>
+      </TabPanels>
+    </Tabs>
+
   </div>
 </template>
 
 <script setup lang="ts">
-
+import Tabs from "primevue/tabs";
+import Tab from "primevue/tab";
+import TabList from "primevue/tablist";
+import TabPanels from "primevue/tabpanels";
+import TabPanel
+  from "primevue/tabpanel";
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.profileDetails {
+  display:flex;
+  flex-wrap: wrap;
+  padding: 1rem;
+  color: $color-text-strong;
+  font-size: 0.8rem;
+}
+
+.dotPadding {
+  padding-left: 0.5rem;
+  padding-right: 0.5rem;
+}
+
+</style>
