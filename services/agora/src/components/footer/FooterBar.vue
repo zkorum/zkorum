92e6f9ddfbf001625f22652cc332865b29edb80f
Edmund Wong <9814980+e32wong@users.noreply.github.com>
2024-10-01 11:42:06 -0400

    Modualize app footer to its own component

---
+++ services/agora/src/components/footer/FooterBar.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="flexIcons">
+    <RouterLink :to="{ name: 'default-home-feed' }">
+      <div class="iconStyle">
+        <q-icon name="mdi-home" size="1.6rem"
+          :color="route.name === 'default-home-feed' ? 'color-highlight' : 'color-text-weak'" />
+        <div :class="'text-' + (route.name === 'default-home-feed' ? 'color-highlight' : 'color-text-weak')">
+          Home
+        </div>
+      </div>
+
+    </RouterLink>
+
+    <RouterLink v-if="authenticationStore.isAuthenticated" :to="{ name: 'user-profile' }">
+      <div class="iconStyle">
+        <q-icon name="mdi-account-circle" size="1.6rem"
+          :color="route.name === 'user-profile' ? 'color-highlight' : 'color-text-weak'" />
+        <div :class="'text-' + (route.name === 'user-profile' ? 'color-highlight' : 'color-text-weak')">
+          Profile
+        </div>
+      </div>
+    </RouterLink>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useAuthenticationStore } from "src/stores/authentication";
+import { useRoute } from "vue-router";
+
+
+const authenticationStore = useAuthenticationStore();
+
+const route = useRoute();
+
+</script>
+
+<style scoped lang="scss">
+.flexIcons {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 2rem;
+}
+
+.iconStyle {
+  padding: 0.3rem;
+  cursor: pointer;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-size: 0.7rem;
+  font-weight: bold;
+}
+</style>
+
+
--- services/agora/src/layouts/MainLayout.vue
+++ services/agora/src/layouts/MainLayout.vue
@@ -9,27 +9,8 @@
           <router-view />
         </q-page-container>
 
-        <q-footer v-if="enableFooter" bordered class="coloredFooter flexIcons">
-          <RouterLink :to="{ name: 'default-home-feed'}">
-            <div class="iconStyle">
-              <q-icon name="mdi-home" size="1.6rem" :color="route.name === 'default-home-feed' ? 'primary' : 'black'" />
-              <div :class="'text-' + (route.name === 'default-home-feed' ? 'primary' : 'black')">
-                Home
-              </div>
-            </div>
-
-          </RouterLink>
-
-          <RouterLink v-if="authenticationStore.isAuthenticated" :to="{ name: 'user-profile' }">
-            <div class="iconStyle">
-              <q-icon name="mdi-account-circle" size="1.6rem"
-                :color="route.name === 'user-profile' ? 'primary' : 'black'" />
-              <div :class="'text-' + (route.name === 'user-profile' ? 'primary' : 'black')">
-                Profile
-              </div>
-            </div>
-          </RouterLink>
-
+        <q-footer v-if="enableFooter" bordered class="coloredFooter">
+          <FooterBar />
         </q-footer>
       </q-layout>
     </WidthWrapper>
@@ -37,16 +18,11 @@
 </template>
 
 <script setup lang="ts">
+import FooterBar from "src/components/footer/FooterBar.vue";
 import WidthWrapper from "src/components/navigation/WidthWrapper.vue";
-import { useAuthenticationStore } from "src/stores/authentication";
 import { MainLayoutProps } from "src/utils/model/props";
-import { useRoute } from "vue-router";
-
-const authenticationStore = useAuthenticationStore();
 
 defineProps<MainLayoutProps>();
-
-const route = useRoute();
 
 </script>
 
@@ -61,23 +37,6 @@ const route = useRoute();
 
 .bottomPagePadding {
   padding-bottom: 10rem;
-}
-
-.flexIcons {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 2rem;
-}
-
-.iconStyle {
-  padding: 0.3rem;
-  cursor: pointer;
-  display:flex;
-  flex-direction: column;
-  align-items: center;
-  font-size: 0.7rem;
-  font-weight: bold;
 }
 
 </style>
