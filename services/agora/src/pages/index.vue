1d7fcc5b051e242f48a590b2a22e8b8024e71cba
Edmund Wong <9814980+e32wong@users.noreply.github.com>
2024-10-01 22:16:14 -0400

    Guard the create new post button with a login check

--- services/agora/src/pages/index.vue
+++ services/agora/src/pages/index.vue
@@ -11,12 +11,21 @@
 <script setup lang="ts">
 import CompactPostList from "src/components/feed/CompactPostList.vue";
 import NewPostButtonWrapper from "src/components/post/NewPostButtonWrapper.vue";
+import { useAuthenticationStore } from "src/stores/authentication";
+import { useDialog } from "src/utils/ui/dialog";
 import { useRouter } from "vue-router";
 
 const router = useRouter();
+const dialog = useDialog();
+
+const authenticationStore = useAuthenticationStore();
 
 function createNewPost() {
-  router.push({ name: "create-post" });
+  if (authenticationStore.isAuthenticated) {
+    router.push({ name: "create-post" });
+  } else {
+    dialog.showLoginConfirmationDialog();
+  }
 }
 
 </script>
