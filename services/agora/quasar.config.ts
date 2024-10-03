e4b7fda99cbb54b75dab5614caf3688e155b953b
Edmund Wong <9814980+e32wong@users.noreply.github.com>
2024-10-03 17:46:44 -0400

    Disable options API

--- services/agora/quasar.config.ts
+++ services/agora/quasar.config.ts
@@ -50,7 +50,7 @@ export default configure((ctx) => {
       vueRouterMode: "history", // available values: 'hash', 'history'
       // vueRouterBase: '/feed/',
       // vueDevtools,
-      // vueOptionsAPI: false,
+      vueOptionsAPI: false,
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
