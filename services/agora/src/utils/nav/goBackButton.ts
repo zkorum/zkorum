58c4ea207160f648f4e7f34eb13e0b77cb7ca0db
Edmund Wong <9814980+e32wong@users.noreply.github.com>
2024-10-15 13:52:32 -0400

    Fix close/back button's behavior

--- services/agora/src/utils/nav/goBackButton.ts
+++ services/agora/src/utils/nav/goBackButton.ts
@@ -6,17 +6,17 @@ export function useGoBackButtonHandler() {
 
   const lastNavigatedRouteName = useStorage("last-navigated-route-name", "");
 
-  const pullUpRouteNameList = ["settings-page", "help-page", "create-post"];
+  // const pullUpRouteNameList = ["settings-page", "help-page", "create-post"];
 
   function goBack() {
     if (lastNavigatedRouteName.value == "") {
       router.push({ name: "default-home-feed" });
     } else {
-      if (pullUpRouteNameList.includes(lastNavigatedRouteName.value)) {
-        router.push({ name: "default-home-feed" });
-      } else {
-        router.back();
-      }
+      // if (pullUpRouteNameList.includes(lastNavigatedRouteName.value)) {
+      //  router.push({ name: "default-home-feed" });
+      // } else {
+      router.back();
+      // }
     }
   }
 
