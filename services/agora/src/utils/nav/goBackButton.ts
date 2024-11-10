09b46529061364fb0fcc2da6004f0c4dd4e39436
Edmund Wong <9814980+e32wong@users.noreply.github.com>
2024-11-10 17:26:53 -0500

    Fix the new post routing's history

--- services/agora/src/utils/nav/goBackButton.ts
+++ services/agora/src/utils/nav/goBackButton.ts
@@ -1,24 +1,11 @@
-import { useRoute, useRouter } from "vue-router";
-import { useLastNavigatedRouteName } from "./lastNavigatedRouteName";
+import { useRouter } from "vue-router";
 
 export function useGoBackButtonHandler() {
   const router = useRouter();
-  const route = useRoute();
-
-  const { lastNavigatedRouteName } = useLastNavigatedRouteName();
 
   function goBack() {
     // const pullUpRouteNameList = ["settings-page", "help-page", "create-post"];
-
-    if (route.name == "single-post") {
-      router.push({ name: "default-home-feed" });
-    } else {
-      if (lastNavigatedRouteName.value == "") {
-        router.push({ name: "default-home-feed" });
-      } else {
-        router.go(-1);
-      }
-    }
+    router.go(-1);
   }
 
   return { goBack };
