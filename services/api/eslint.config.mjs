f0409d9291e39110d14bb6a5dcafd682676af984
Nicolas Gimenez <46368291+nicobao@users.noreply.github.com>
2024-11-18 10:16:50 +0100

    feat: improve eslint & prettier config

--- services/api/eslint.config.mjs
+++ services/api/eslint.config.mjs
@@ -2,6 +2,7 @@
 
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default tseslint.config(
     eslint.configs.recommended,
@@ -33,4 +34,5 @@ export default tseslint.config(
             "drizzle.config.ts",
         ],
     },
+    eslintConfigPrettier, // eslint-config-prettier last
 );
