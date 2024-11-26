78b72e025012f87c1509271122741f9b78bbb873
Nicolas Gimenez <46368291+nicobao@users.noreply.github.com>
2024-11-26 14:39:43 +0100

    fix: rebase mes:s

--- services/agora/src/utils/api/user.ts
+++ services/agora/src/utils/api/user.ts
@@ -1,4 +1,9 @@
-import { DefaultApiAxiosParamCreator, DefaultApiFactory, type ApiV1UserFetchUserCommentsPostRequest, type ApiV1UserFetchUserPostsPostRequest } from "src/api";
+import {
+  DefaultApiAxiosParamCreator,
+  DefaultApiFactory,
+  type ApiV1UserFetchUserCommentsPostRequest,
+  type ApiV1UserFetchUserPostsPostRequest,
+} from "src/api";
 import { api } from "boot/axios";
 import { buildAuthorizationHeader } from "../crypto/ucan/operation";
 import { useCommonApi } from "./common";
@@ -27,55 +32,25 @@ export function useBackendUserApi() {
         },
       });
 
-<<<<<<< HEAD
-||||||| parent of 31f7843 (chore: prepare for staging deployment)
-      const internalPostList: ExtendedPost[] = response.data.userPostList.map((postElement) => {
-        const dataItem: ExtendedPost = createInternalPostData(postElement);
-        return dataItem;
-      });
-
-=======
-      const internalPostList: ExtendedPost[] = response.data.userPostList.map(
-        (postElement) => {
-          const dataItem: ExtendedPost = createInternalPostData(postElement);
-          return dataItem;
-        }
-      );
-
->>>>>>> 31f7843 (chore: prepare for staging deployment)
       return {
         commentCount: response.data.commentCount,
         postCount: response.data.postCount,
         createdAt: new Date(response.data.createdAt),
         userName: response.data.userName,
-<<<<<<< HEAD
-||||||| parent of 31f7843 (chore: prepare for staging deployment)
-        userPostList: internalPostList
-=======
-        userPostList: internalPostList,
->>>>>>> 31f7843 (chore: prepare for staging deployment)
       };
     } catch (e) {
       console.error(e);
-<<<<<<< HEAD
       showNotifyMessage("Failed to fetch user's personal profile.");
-||||||| parent of 31f7843 (chore: prepare for staging deployment)
-      showMessage("An error had occured", "Failed to fetch user's personal votes.");
-=======
-      showMessage(
-        "An error had occured",
-        "Failed to fetch user's personal votes."
-      );
->>>>>>> 31f7843 (chore: prepare for staging deployment)
       return undefined;
     }
   }
 
-<<<<<<< HEAD
-  async function fetchUserPosts(lastPostSlugId: string | undefined): Promise<ExtendedPost[]> {
+  async function fetchUserPosts(
+    lastPostSlugId: string | undefined
+  ): Promise<ExtendedPost[]> {
     try {
       const params: ApiV1UserFetchUserPostsPostRequest = {
-        lastPostSlugId: lastPostSlugId
+        lastPostSlugId: lastPostSlugId,
       };
 
       const { url, options } =
@@ -91,10 +66,12 @@ export function useBackendUserApi() {
         },
       });
 
-      const internalPostList: ExtendedPost[] = response.data.map((postElement) => {
-        const dataItem: ExtendedPost = createInternalPostData(postElement);
-        return dataItem;
-      });
+      const internalPostList: ExtendedPost[] = response.data.map(
+        (postElement) => {
+          const dataItem: ExtendedPost = createInternalPostData(postElement);
+          return dataItem;
+        }
+      );
 
       return internalPostList;
     } catch (e) {
@@ -104,14 +81,18 @@ export function useBackendUserApi() {
     }
   }
 
-  async function fetchUserComments(lastCommentSlugId: string | undefined): Promise<ExtendedComment[]> {
+  async function fetchUserComments(
+    lastCommentSlugId: string | undefined
+  ): Promise<ExtendedComment[]> {
     try {
       const params: ApiV1UserFetchUserCommentsPostRequest = {
-        lastCommentSlugId: lastCommentSlugId
+        lastCommentSlugId: lastCommentSlugId,
       };
 
       const { url, options } =
-        await DefaultApiAxiosParamCreator().apiV1UserFetchUserCommentsPost(params);
+        await DefaultApiAxiosParamCreator().apiV1UserFetchUserCommentsPost(
+          params
+        );
       const encodedUcan = await buildEncodedUcan(url, options);
       const response = await DefaultApiFactory(
         undefined,
@@ -124,7 +105,7 @@ export function useBackendUserApi() {
       });
 
       const extendedCommentList: ExtendedComment[] = [];
-      response.data.forEach(responseItem => {
+      response.data.forEach((responseItem) => {
         const extendedComment: ExtendedComment = {
           postData: createInternalPostData(responseItem.postData),
           commentItem: {
@@ -134,14 +115,13 @@ export function useBackendUserApi() {
             numDislikes: responseItem.commentItem.numDislikes,
             numLikes: responseItem.commentItem.numLikes,
             updatedAt: new Date(responseItem.commentItem.updatedAt),
-            userName: responseItem.commentItem.userName
-          }
-        }
+            userName: responseItem.commentItem.userName,
+          },
+        };
         extendedCommentList.push(extendedComment);
       });
 
       return extendedCommentList;
-
     } catch (e) {
       console.error(e);
       showNotifyMessage("Failed to fetch user's personal comments.");
@@ -150,10 +130,4 @@ export function useBackendUserApi() {
   }
 
   return { fetchUserProfile, fetchUserPosts, fetchUserComments };
-||||||| parent of 31f7843 (chore: prepare for staging deployment)
-
-  return { fetchUserProfile };
-=======
-  return { fetchUserProfile };
->>>>>>> 31f7843 (chore: prepare for staging deployment)
 }
