90a335ce79f5efdb70934e0c344f0a83c1ac8e7c
Nicolas Gimenez <46368291+nicobao@users.noreply.github.com>
2024-12-06 03:34:17 +0100

    feat: add deployment config for Rarimo integration
    This commit, along with the integration of the Rarimo protocol into Agora in general, was developed with funding from the European Union’s Horizon Europe 2020 research and innovation program, as part of the NGI SARGASSO project under grant agreement No. 101092887.

---
+++ script/rarimo/migrate-db-verificator.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+docker run --network=zkorum_default -e KV_VIPER_FILE=/usr/local/etc/config.yaml -v ./script/rarimo/config/config.yaml:/usr/local/etc/config.yaml zkorum/verificator-svc:0.2.2 migrate up
